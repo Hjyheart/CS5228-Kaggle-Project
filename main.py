5d44f840d88c78ca338bc48c6b02b39ff737a578
Hjyheart <jhongag@connect.ust.hk>
2024-11-09 21:38:06 +0800

    clean up code base

--- main.py
+++ main.py
@@ -1,21 +1,10 @@
-import sys
-import os
-import difflib
-import datetime
-import dateutil
 import xgboost
-import numpy as np
 import pandas as pd
-import scipy.stats as stats
-import category_encoders as ce
 
-from sklearn.impute import KNNImputer
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import RandomizedSearchCV
 
-sys.path.insert(0, os.getcwd() + '/script/')
-import carly
+from script import carly
 
 def model_preprocessing(input_df):
     
@@ -97,72 +86,36 @@ elif cat_encoding == True:
     enable_categorical  = False
     model_name          = 'ENCODED_xgboost'
 
-####################### hyperparameters tuning ######################
-#####################################################################
-
-# Define the hyperparameter distributions
-param_dist = {
-            'learning_rate'         : stats.uniform(0.01, 0.1),
-            'gamma'                 : stats.uniform(0.01, 0.9),    
-            'alpha'                 : stats.uniform(0.01, 0.1),
-            'subsample'             : stats.uniform(0.3, 0.7),
-            'colsample_bytree'      : stats.uniform(0.3, 0.7),
-            'early_stopping_rounds' : stats.randint(8, 18), 
-            'min_child_weight'      : stats.randint(3, 88),   
-            }
-
-xgb_model = xgboost.XGBRegressor(random_state       = 888888, 
-                                 n_estimators       = 1888,
-                                 max_depth          = 5,              
-                                 enable_categorical = enable_categorical,
-                                 n_jobs             = -1,
-                                 )
-
-random_search = RandomizedSearchCV(xgb_model, 
-                                   param_distributions  = param_dist, 
-                                   n_iter               = 88, 
-                                   cv                   = 5, 
-                                   random_state         = 888888,
-                                   scoring              = 'neg_root_mean_squared_error',)
-
-random_search.fit(train_df_base[feature_list], train_df_base[target], eval_set = [(train_df_eval[feature_list], train_df_eval[target],)], verbose = False)
-random_search_df = pd.DataFrame(random_search.cv_results_)
-random_search_df = random_search_df[['params',
-                                    'split0_test_score',
-                                    'split1_test_score',
-                                    'split2_test_score',
-                                    'split3_test_score',
-                                    'split4_test_score',
-                                    'mean_test_score',
-                                    'std_test_score',
-                                    'rank_test_score']]
-
-random_search_df['objfunc'] = random_search_df[['split0_test_score',
-                                                'split1_test_score',
-                                                'split2_test_score',
-                                                'split3_test_score',
-                                                'split4_test_score',]].median(axis=1)
-random_search_df = random_search_df.sort_values('objfunc', ascending = False)
-best_params = random_search_df['params'].iloc[0]
-
 ####################### model training  ####################################
 ############################################################################
+# Use the best_params from main-bayesian
+best_params ={
+    'alpha': 0.07272769267361982,
+    'colsample_bytree': 0.5004695925119211,
+    'early_stopping_rounds': 18,
+    'gamma': 0.33307638017245794,
+    'learning_rate': 0.014079290196807495,
+    'max_depth': 52,
+    'min_child_weight': 10,
+    'n_estimators': 1123,
+    'subsample': 0.575409364755193
+}
 
-xbg_model = xgboost.XGBRegressor(random_state          = 888888,
-                                n_estimators           = 1888,
-                                enable_categorical     = True,
-                                n_jobs                 = -1,
-                                max_depth              = 5,
-                                early_stopping_rounds  = best_params['early_stopping_rounds'],
-                                
-                                learning_rate          = best_params['learning_rate'], #  step size shrinkage used in updates to prevent overfitting. Lower values make the model more robust by taking smaller steps.
-                                gamma                  = best_params['gamma'], #minimum loss reduction required to make a further partition on a leaf node of the tree. Higher values increase the regularization.
-                                alpha                  = best_params['alpha'], #L2 regularization term on weights. 
-                                
-                                subsample              = best_params['subsample'], #percentage of rows used for each tree construction. 
-                                colsample_bytree       = best_params['colsample_bytree'], #percentage of columns used for each tree construction
-                                min_child_weight       = best_params['min_child_weight'], #Minimum sum of instance weight (hessian) needed in a child. If the tree partition step results in a leaf node with the sum of instance weight less than min_child_weight, then the building process will give up further partitioning.
-                                verbosity = 2
+
+xbg_model = xgboost.XGBRegressor(random_state           = 888888,
+                                 n_estimators           = best_params['n_estimators'],
+                                 n_jobs                 = -1,
+                                 max_depth              = best_params['max_depth'],
+                                 early_stopping_rounds  = best_params['early_stopping_rounds'],
+
+                                 learning_rate          = best_params['learning_rate'], #  step size shrinkage used in updates to prevent overfitting. Lower values make the model more robust by taking smaller steps.
+                                 gamma                  = best_params['gamma'], #minimum loss reduction required to make a further partition on a leaf node of the tree. Higher values increase the regularization.
+                                 alpha                  = best_params['alpha'], #L2 regularization term on weights.
+
+                                 subsample              = best_params['subsample'], #percentage of rows used for each tree construction.
+                                 colsample_bytree       = best_params['colsample_bytree'], #percentage of columns used for each tree construction
+                                 min_child_weight       = best_params['min_child_weight'], #Minimum sum of instance weight (hessian) needed in a child. If the tree partition step results in a leaf node with the sum of instance weight less than min_child_weight, then the building process will give up further partitioning.
+                                 verbosity = 2
                                 )
 
 xbg_model.fit(train_df_base[feature_list], train_df_base[target], eval_set=[(train_df_eval[feature_list], train_df_eval[target],)], verbose = False)
@@ -172,4 +125,4 @@ print(f"FINAL EVAL RMSE SCORE: {eval_rmse}")
 
 submission_df = pd.DataFrame(xbg_model.predict(test_df[feature_list])).reset_index().rename(columns = {'index':"Id", 0:'Predicted'})
 submission_df['Predicted'] = round(submission_df['Predicted'])
-submission_df.to_csv(fr"./data/{model_name}_submission.csv", index = False)
+submission_df.to_csv(fr"./dataset/{model_name}_submission.csv", index = False)
